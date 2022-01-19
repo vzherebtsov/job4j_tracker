1dd1d626509a0791777903dec4a4c1fc3f83e888
Vsevolod <seva_zherebtsov@mail.ru>
2022-01-19 16:43:18 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,29 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println(active);
+        System.out.println(status);
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        Error err = new Error(true, 1, "err");
+        error.printInfo();
+        err.printInfo();
+    }
+}

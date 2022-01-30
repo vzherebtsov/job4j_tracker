2017b424a0838ce4ec78226d41317147614b08d3
Vsevolod <seva_zherebtsov@mail.ru>
2022-01-30 19:53:49 +0300

    6. Date. Отображение даты. [#310017]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,15 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item item = new Item();
+        item.getCreated();
+        LocalDateTime created = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String currentDateTimeFormat = created.format(formatter);
+        System.out.println(currentDateTimeFormat);
+    }
+}

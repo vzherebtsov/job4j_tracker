af8dcd983ca64019aa561965794124b0fbec0302
Vsevolod <seva_zherebtsov@mail.ru>
2022-02-06 15:04:18 +0300

    5. Tracker - хранилище [#396 #260393]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -14,16 +14,7 @@ public class Tracker {
     }
 
     public Item[] findAll() {
-        Item[] rsl = new Item[size];
-        int count = 0;
-        for (int i = 0; i < size; i++) {
-            Item name = items[i];
-            if (name != null) {
-                rsl[count] = name;
-                count++;
-            }
-        }
-        return Arrays.copyOf(rsl, count);
+        return Arrays.copyOf(items, size);
     }
 
     public Item[] findByName(String key) {

ba2a21e11e79a99f249c0acd893c46667e017c98
Vsevolod <seva_zherebtsov@mail.ru>
2022-03-07 20:26:00 +0300

    3. Интерфейс Input [#181092 #271064]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -4,10 +4,12 @@ public class Bus implements Transport {
 
     @Override
     public void go() {
+        System.out.println("Автобус едет");
     }
 
     @Override
     public void passengers(int number) {
+        System.out.println("Количество пассажиров: " + number);
     }
 
     @Override

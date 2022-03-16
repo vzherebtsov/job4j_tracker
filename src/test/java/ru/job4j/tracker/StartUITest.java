abe9c60fd58f60117750a0b537710e1610977f89
Vsevolod <seva_zherebtsov@mail.ru>
2022-03-16 15:16:10 +0300

    8. Реализация меню за счет шаблона стратегия. [#181780 #276043]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,10 +1,5 @@
 package ru.job4j.tracker;
 
-import org.junit.Test;
-import static org.hamcrest.core.Is.is;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.IsNull.nullValue;
-
 public class StartUITest {
 
 }

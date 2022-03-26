24107879e753dde16d89650ce5fc6c1209a7e9d3
Vsevolod <seva_zherebtsov@mail.ru>
2022-03-26 17:19:09 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,24 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                rsl = i;
+            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("Элемент не найден");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        try {
+            indexOf(new String[]{"qq", "ww", "ee"}, "q");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}

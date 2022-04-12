85ac23ef2ce4e304f36a71aed6c51f069738f316
Vsevolod <seva_zherebtsov@mail.ru>
2022-04-12 13:18:19 +0300

    1. Singleton [#94606]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -35,7 +35,7 @@ public class Tracker {
         return index != -1 ? items[index] : null;
     }
 
-    private int indexOf(int id) {
+    public int indexOf(int id) {
         int rsl = -1;
         for (int index = 0; index < size; index++) {
             if (items[index].getId() == id) {

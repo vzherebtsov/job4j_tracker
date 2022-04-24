b97a9ca4b684cbc51f320c8dcad62ca120e2a4f5
Vsevolod <seva_zherebtsov@mail.ru>
2022-04-24 14:07:36 +0300

    2. final [#334073]

--- src/main/java/ru/job4j/ooa/Airport.java
+++ src/main/java/ru/job4j/ooa/Airport.java
@@ -2,12 +2,12 @@ package ru.job4j.ooa;
 
 public class Airport {
     public static void main(String[] args) {
-        Airbus airbus = new Airbus("A320");
+        final Airbus airbus = new Airbus("A320");
         System.out.println(airbus);
         airbus.printModel();
         airbus.printCountEngine();
 
-        airbus = new Airbus("A380");
+        airbus.setName("A380");
         System.out.println(airbus);
         airbus.printModel();
         airbus.printCountEngine();

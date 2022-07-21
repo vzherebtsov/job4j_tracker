1890cd3152589739ff0b1b3a04fd94fd71f1452c
Vsevolod <seva_zherebtsov@mail.ru>
2022-07-21 13:03:50 +0300

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,14 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("seva_zherebtsov@mail.ru", "Zherebtsov Vsevolod Vladimirovich");
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}

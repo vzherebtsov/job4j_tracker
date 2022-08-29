7d27d5d558021d0b8e524be044203b9bf5a565b5
Vsevolod <seva_zherebtsov@mail.ru>
2022-08-29 17:52:09 +0300

    2. Отсортировать департаменты [#34352]

---
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -0,0 +1,16 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class DepDescComp implements Comparator<String> {
+    @Override
+    public int compare(String o1, String o2) {
+        String[] first = o1.split("/");
+        String[] second = o2.split("/");
+        int rsl = second[0].compareTo(first[0]);
+        if (rsl == 0) {
+            rsl = o1.compareTo(o2);
+        }
+        return rsl;
+    }
+}
